665da3040d3b14866d1bd0e626cb8b3f1bcfb295
Pablo Marelli <pablo.marelli.kenny@gmail.com>
2026-02-28 11:51:57 -0300

    Testing the pipeline.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Something new here</h1>
+      <h1>Testing the pipeline</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
